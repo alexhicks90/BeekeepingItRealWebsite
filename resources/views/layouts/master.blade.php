11e61acefc701e9daa4818cf661fd0707b6f1087
Alex <=ahick090@gmail.com>
2018-10-02 19:30:22 -0400

    Add logo icon

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,6 +10,7 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
   <link href="https://fonts.googleapis.com/css?family=Oswald:700|Patua+One|Roboto+Condensed:700" rel="stylesheet">
   <link rel="stylesheet" href="/css/main.css">
+  <link rel="shortcut icon" href="images/logo.ico">
   @yield ('assets')
   <title>Beekeeping It Real!</title>
 </head>
